73085875d92d1fa3ef3ee4bb55cae178185cf84b
Tobiwit <76102470+Tobiwit@users.noreply.github.com>
2022-09-15 19:02:42 +0200

    Started implementing Account registration and sign in

--- app/src/indexLogin.js
+++ app/src/indexLogin.js
@@ -7,24 +7,47 @@ function setupServerConnection() {
     client
         .setEndpoint("https://appwrite.software-engineering.education/v1") // Your API Endpoint
         .setProject("62f0ca546a9a137d9df7");
+}
 
+function loginUserSession(email, password) {
+    account.createEmailSession(email, password)
+        .then(response => {
+            console.log(response);
+            setCookie("userId", response.userId);
+        }, error => {
+            console.log(error);
+        });
+}
+
+function createUser(email, password, name) {
     // Register User
-    account.create('unique()', 'me@example.com', 'password', 'Tobi')
-            .then(response => {
-                console.log(response);
-            }, error => {
-                console.log(error);
-            });
+    account.create('unique()', email, password, name)
+        .then(response => {
+            console.log(response);
+            setCookie("userId", response.$id);
+        }, error => {
+            console.log(error);
+        });
 }
 
 function init() {
     setupServerConnection();
-    let button = document.querySelector("button");
-    button.onclick = function() {
-        window.location.replace("./pageMain.html");
+    let signUpButton = document.querySelector(".signUp"),
+        signInButton = document.querySelector(".signIn"),
+        inputEmail = document.querySelector(".email"),
+        inputName = document.querySelector(".name"),
+        inputPassword = document.querySelector(".password");
+    signInButton.onclick = function() {
+        loginUserSession(inputEmail.value, inputPassword.value);
     };
-    
 
+    signUpButton.onclick = function() {
+        createUser(inputEmail.value, inputPassword.value, inputName.value);
+    };
+}
+
+function setCookie(name, value) {
+    document.cookie = name + "=" + value;
 }
 
 init();
